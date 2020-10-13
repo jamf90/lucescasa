e752b5fa2d9d78fee8cdb254b8dc365cc5342021
Nilver92 <70180099+Nilver92@users.noreply.github.com>
2020-10-12 20:12:23 -0500

    Asigno credenciales

--- web/index.php
+++ web/index.php
@@ -35,15 +35,15 @@ $app->post('/enviarDato', function (Request $request) use ($app) {
 //Ruta de demostración, se recibe(n) dato(s) y se manipulan
 $app->post('/guardarDato', function (Request $request) use ($app) {
 
-	$temperature = $request->get('temperature');
+	$Voltbat = $request->get('Voltbat');
 	$tabla = $request->get('tabla');
 
-	$dbconn = pg_pconnect("host=ec2-52-21-0-111.compute-1.amazonaws.com port=5432 dbname=da23ojrg1de3ae user=msmhlrvxhgltyv password=baf2024024b59cdd7b5bd1a44e8d8a7773810a5ccbce3719f01225c9baac9bf2");
+	$dbconn = pg_pconnect("host=ec2-54-152-40-168.compute-1.amazonaws.com port=5432 dbname=da5l2p8fhao45b user=rvjdadbcfsozcx password=d568c86e4a84d477292656b6718984c408f607f5459bca9b6eaf550604dfcf66;
 
 	$data = array(
-		"fecha"=>date('Y-m-d H:i:s'),
-		"placeSense" => $request->get('lugar'),
-		"temperature" => $temperature
+		"Fecha"=>date('Y-m-d H:i:s'),
+		"Voltpanel" => $request->get('Voltpanel'),
+		"Voltbat" => $Voltbat
 		);
 
 	$respuesta = pg_insert($dbconn, $tabla, $data);
