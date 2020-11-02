c5e08e4cb5590a7df348ec12dcd0a16d6037e793
jamf90 <70180507+jamf90@users.noreply.github.com>
2020-11-02 10:05:13 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 date_default_timezone_set('America/Bogota');
 
 require('../vendor/autoload.php');
